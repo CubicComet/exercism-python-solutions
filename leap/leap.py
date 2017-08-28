61108f794b53048aae3fc9840abf34a4150bb83a
CubicComet <cometa2k7@gmail.com>
2017-08-28 21:00:56 +0100

    Combine tests into one

--- leap/leap.py
+++ leap/leap.py
@@ -1,9 +1,2 @@
 def is_leap_year(y):
-    if y % 400 == 0:
-        return True
-    elif y % 100 == 0:
-        return False
-    elif y % 4 == 0:
-        return True
-    else:
-        return False
+    return (y % 4 == 0) and (y % 100 != 0 or y % 400 == 0)
