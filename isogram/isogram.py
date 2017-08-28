230120ee1ebeaad7632e1cf2d0c964e3670f06e1
Cubic Comet <cometa2k7@gmail.com>
2017-08-28 22:30:22 +0100

    Add note about str.isalpha() method as an alternative

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -7,3 +7,7 @@ LOWERCASE = set(ascii_lowercase)
 def is_isogram(s):
     chars = [c for c in s.lower() if c in LOWERCASE]
     return len(chars) == len(set(chars))
+
+
+# You could also achieve this using "c.isalpha()" instead of LOWERCASE
+# You would then not need to import from `string`, but it's marginally slower
