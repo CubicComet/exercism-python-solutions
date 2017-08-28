9a0fa307b92505b6b12252d2ee4c594dde37f37e
Cubic Comet <cometa2k7@gmail.com>
2017-08-28 21:31:05 +0100

    Add an alternative one-liner

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,5 @@
 def is_leap_year(y):
-    return (y % 4 == 0) and (y % 100 != 0 or y % 400 == 0)
+    return (y % 400 == 0) or (y % 4 == 0 and y % 100 != 0)
+
+# Alternatively, we can use the below, but it's slightly slower.
+#    return (not y % 400) or (bool(y % 4)^bool(y % 100))
