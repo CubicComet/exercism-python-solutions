abc0a9860aaa556051bd79a0b854fa088b3a149c
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-08-28 23:47:58 +0100

    Add initial solution

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,11 @@
-def to_rna():
-    pass
+DNA = {"A", "C", "T", "G"}
+
+TRANS = {"G": "C", "C":"G", "T":"A", "A":"U"}
+
+
+def to_rna(dna):
+    # Check validity - `difference` returns elements in dna not in DNA
+    if set(dna).difference(DNA):
+        return ""
+    
+    return "".join([TRANS[n] for n in dna])
