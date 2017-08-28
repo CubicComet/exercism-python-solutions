e6e6684e7bf2858778491d6e73fe351082c8bfea
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-08-28 22:10:09 +0100

    Add initial solution

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,9 @@
-def is_isogram(string):
-    pass
+from string import ascii_lowercase
+
+
+LOWERCASE = set(ascii_lowercase)
+
+
+def is_isogram(s):
+    chars = [c for c in s.lower() if c in LOWERCASE]
+    return len(chars) == len(set(chars))
