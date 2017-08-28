4a54138dd70f57b87f5ae47e7d04ca6cebbe517e
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-08-29 00:20:54 +0100

    Add an exception based version

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,9 +1,20 @@
-DNA = {"A", "C", "T", "G"}
-
 TRANS = {"G": "C", "C":"G", "T":"A", "A":"U"}
 
 
 def to_rna(dna):
+    try:
+        return "".join([TRANS[n] for n in dna])
+    except KeyError:
+        return ""
+
+
+# Old version: it's slightly slower for valid DNA, but slightly faster for invalid DNA
+
+DNA = {"A", "C", "T", "G"}
+TRANS = {"G": "C", "C":"G", "T":"A", "A":"U"}
+
+
+def to_rna_old(dna):
     # Check validity - `difference` returns elements in dna not in DNA
     if set(dna).difference(DNA):
         return ""
