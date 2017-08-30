39c14f0e1246741aba692ec8c3f0f2c75bc49310
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-08-30 20:45:02 +0100

    Add initial solution

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,10 @@
-def hey(string):
-    pass
+def hey(s):
+    s = s.strip()
+    if s.isupper():
+        return "Whoa, chill out!"
+    elif s == "":
+        return "Fine. Be that way!"
+    elif s[-1] == "?":
+        return "Sure."
+    else:
+        return "Whatever."
