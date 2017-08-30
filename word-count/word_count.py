6b31ee7076f18ad20e70838b41c9e3a3557ec6df
CubicComet <cometa2k7@gmail.com>
2017-08-30 11:42:28 +0100

    Initial solution

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,8 @@
-def word_count():
-    pass
+def word_count(s):
+    words = strip_punc(s.lower()).split()
+    return {word: words.count(word) for word in set(words)}
+
+
+
+def strip_punc(s):
+    return "".join(ch if ch.isalnum() else " " for ch in s)
