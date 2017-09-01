ed4f51db1ed75a1ea0f94adf1fbb5649ad5ca013
CubicComet <cometa2k7@gmail.com>
2017-09-01 18:39:12 +0100

    Add initial solution

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,23 @@
-def rotate():
-    pass
+import string
+
+
+UPPER = string.ascii_uppercase
+LOWER = string.ascii_lowercase
+
+
+def rotate(s, n):
+    return "".join(rot_gen(s,n))
+
+
+def shift_rules(n):
+    shifted = UPPER[n:] + UPPER[:n] + LOWER[n:] + LOWER[:n]
+    return {k:v for k,v in zip(UPPER+LOWER, shifted)}
+
+
+def rot_gen(s, n):
+    rules = shift_rules(n)
+    for ch in s:
+        try:
+            yield rules[ch]
+        except KeyError:
+            yield ch
