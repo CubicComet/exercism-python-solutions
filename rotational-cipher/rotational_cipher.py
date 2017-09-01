fc2085e3c86e1596f5dc9c032e445887430602b5
Cubic Comet <cometa2k7@gmail.com>
2017-09-01 19:56:10 +0100

    Use lambda function with  method

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -6,18 +6,10 @@ LOWER = string.ascii_lowercase
 
 
 def rotate(s, n):
-    return "".join(rot_gen(s,n))
+    rules = shift_rules(n)
+    return "".join(map(lambda k: rules.get(k, k), s))
 
 
 def shift_rules(n):
     shifted = UPPER[n:] + UPPER[:n] + LOWER[n:] + LOWER[:n]
     return {k:v for k,v in zip(UPPER+LOWER, shifted)}
-
-
-def rot_gen(s, n):
-    rules = shift_rules(n)
-    for ch in s:
-        try:
-            yield rules[ch]
-        except KeyError:
-            yield ch
