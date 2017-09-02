5075f44e6e1f53ae0dbfd7e18a6e6f08ceeaea16
Cubic Comet <cometa2k7@gmail.com>
2017-09-03 00:22:48 +0100

    Add initial solution

--- robot-simulator/robot_simulator.py
+++ robot-simulator/robot_simulator.py
@@ -1,3 +1,43 @@
+import math
+
+# Create globals for the bearings
+EAST = 0
+NORTH = 90
+WEST = 180
+SOUTH = 270
+
+
+# Define simpler int-only sine and cosine functions
+def sin(deg):
+    return int(math.sin(math.radians(deg)))
+
+def cos(deg):
+    return int(math.cos(math.radians(deg)))
+
+
 class Robot(object):
-    def __init__(self):
-        pass
+    def __init__(self, bearing=NORTH, x_pos=0, y_pos=0):
+        self.bearing = bearing
+        self._x = x_pos
+        self._y = y_pos
+        self._METHODS = {"R": self.turn_right,
+                        "L": self.turn_left,
+                        "A": self.advance}
+
+    def turn_right(self):
+        self.bearing = (self.bearing - 90) % 360
+
+    def turn_left(self):
+        self.bearing = (self.bearing + 90) % 360
+
+    def advance(self):
+        self._x += cos(self.bearing)
+        self._y += sin(self.bearing)
+
+    def simulate(self, commands):
+        for command in commands:
+            self._METHODS[command]()
+
+    @property
+    def coordinates(self):
+        return (self._x, self._y)
