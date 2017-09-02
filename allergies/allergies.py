77cb20664cb0580eb33471e57dc5d1431d49ec69
CubicComet <cometa2k7@gmail.com>
2017-09-02 17:55:34 +0100

    Add initial solution

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,11 +1,15 @@
 class Allergies(object):
 
+    ALLERGENS = {1:"eggs", 2:"peanuts", 3:"shellfish", 4:"strawberries",
+                 5:"tomatoes", 6:"chocolate", 7:"pollen", 8:"cats"}
+
     def __init__(self, number):
-        pass
+        self.number = number
 
     def is_allergic_to(self, string):
-        pass
+        return string in self.lst
 
     @property
     def lst(self):
-        pass
+        return [Allergies.ALLERGENS[k+1] for k in range(8)
+                if self.number & (1 << k)]
