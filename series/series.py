c17f2cd43597b435853a8b21fc2800bddb644dd9
Cubic Comet <cometa2k7@gmail.com>
2017-09-02 22:50:27 +0100

    Add initial solution

--- series/series.py
+++ series/series.py
@@ -1,2 +1,8 @@
-def slices():
-    pass
+def slices(s, n):
+    m = len(s)
+    if n > m:
+        raise ValueError("Slice cannot be larger than the string")
+    if n < 1:
+        raise ValueError("Slices must have a positive non-zero length")
+        
+    return [list(map(int, s[i:i+n])) for i in range(m-n+1)]
