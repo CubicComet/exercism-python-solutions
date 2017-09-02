d98199be9e5eadbecfb5d116913309290b210c34
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-09-02 16:37:02 +0100

    Add initial solution

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,14 @@
-def square_of_sum():
-    pass
+def square_of_sum(n):
+    return square(sum(range(n+1)))
 
 
-def sum_of_squares():
-    pass
+def sum_of_squares(n):
+    return sum(map(square, range(n+1)))
 
 
-def difference():
-    pass
+def difference(n):
+    return square_of_sum(n) - sum_of_squares(n)
+
+
+def square(n):
+    return n*n
