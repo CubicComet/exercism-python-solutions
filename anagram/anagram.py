29d2bf223e717edea776d5abde6c5895786eaab6
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-09-02 17:15:18 +0100

    Add initial solution

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,7 @@
-def detect_anagrams(string, array):
-    pass
+def detect_anagrams(s, array):
+    return [cand for cand in array if is_anagram(cand, s)]
+
+def is_anagram(s1, s2):
+    s1 = s1.lower()
+    s2 = s2.lower()
+    return s1 != s2 and sorted(s1) == sorted(s2)
