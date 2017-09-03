ba47a45a3f93f58a67b978099f3bea518217f8e2
CubicComet <cometa2k7@gmail.com>
2017-09-03 22:32:08 +0100

    Add intial solution

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,3 @@
-def sum_of_multiples():
-    pass
+def sum_of_multiples(limit, factors):
+    return sum(filter(lambda n: n < limit,
+                      {f*i for i in range(1, limit+1) for f in factors}))
