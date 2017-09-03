e025deac969c5a46d28c18f95194e1220276ccb3
Cubic Comet <cometa2k7@gmail.com>
2017-09-03 01:48:28 +0100

    Add initial solution

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,2 +1,13 @@
-def sieve():
-    pass
+def sieve(n):
+    return list(primes(n))
+
+
+def primes(n):
+    if n < 2:
+        raise StopIteration
+    yield 2
+    not_prime = set()
+    for i in range(3, n+1, 2):
+        if i not in not_prime:
+            yield i
+        not_prime.update(range(i, n, i))
