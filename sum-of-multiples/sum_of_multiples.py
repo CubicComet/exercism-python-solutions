bd6b969b85a1c8df7cf8d6da7b93f5c94cf8a180
CubicComet <cometa2k7@gmail.com>
2017-09-03 22:40:49 +0100

    Use more optimal method of getting multiples

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,3 +1,2 @@
 def sum_of_multiples(limit, factors):
-    return sum(filter(lambda n: n < limit,
-                      {f*i for i in range(1, limit) for f in factors}))
+    return sum({n for f in factors for n in range(f, limit, f)})
