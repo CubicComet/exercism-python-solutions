a5fd77c04fb224c64e6d9b234c32ff989fee21ee
Cubic Comet <cometa2k7@gmail.com>
2017-09-03 22:35:09 +0100

    Remove unnecessary  on

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,3 +1,3 @@
 def sum_of_multiples(limit, factors):
     return sum(filter(lambda n: n < limit,
-                      {f*i for i in range(1, limit+1) for f in factors}))
+                      {f*i for i in range(1, limit) for f in factors}))
