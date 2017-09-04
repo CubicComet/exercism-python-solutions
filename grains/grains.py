54c55c85eb73fd9d80bad8b536a75758e325c765
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-09-05 00:12:15 +0100

    Add initial solution

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,12 @@
-def on_square():
-    pass
+def on_square(n):
+    if n < 65:
+        return 1 << (n - 1)
+    else:
+        raise ValueError
 
 
-def total_after():
-    pass
+def total_after(n):
+    if n and n < 65:
+        return (1 << n) - 1
+    else:
+        raise ValueError
