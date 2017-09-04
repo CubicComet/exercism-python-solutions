fce3d19548f9f7f67ae91c595a06cbd478435e28
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-09-04 22:45:39 +0100

    Initial solution

--- roman-numerals/roman_numerals.py
+++ roman-numerals/roman_numerals.py
@@ -1,2 +1,21 @@
-def numeral():
-    pass
+def numeral(arabic):
+    m, rem = divmod(arabic, 1000)
+    d, rem = divmod(rem, 500)
+    c, rem = divmod(rem, 100)
+    l, rem = divmod(rem, 50)
+    x, rem = divmod(rem, 10)
+    v, i = divmod(rem, 5)
+
+    numerals = m * "M"
+    numerals += _numeral((c, d), ("C", "D", "M"))
+    numerals += _numeral((x, l), ("X", "L", "C"))
+    numerals += _numeral((i, v), ("I", "V", "X"))
+    return numerals
+
+
+def _numeral(nums, letters):
+    i, v, x = letters
+    if nums[1] == 1:
+        return i + x if nums[0] == 4 else v + nums[0] * i
+    else:
+        return i + v if nums[0] == 4 else nums[0] * i
