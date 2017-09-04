4739524c9a5f54bd01e2ac597d672af07fc8e9ee
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-09-04 17:13:08 +0100

    Add initial solution

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,16 @@
+CHILDREN = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred",
+            "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"]
+PLANTS = {"C": "Clover", "G": "Grass", "R": "Radishes", "V": "Violets"}
+
+
 class Garden(object):
-    def __init__(self):
-        pass
+    def __init__(self, garden, students=CHILDREN):
+        self.students = sorted(students)
+        rows = garden.split()
+        patches = [rows[0][i:i+2] + rows[1][i:i+2]
+                   for i in range(0,2*len(self.students),2)]
+        self._garden = {s: [PLANTS[ch] for ch in p]
+                        for s, p in zip(self.students, patches)}
+
+    def plants(self, student):
+        return self._garden[student]
