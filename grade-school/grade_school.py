d000f21e1496584ba2839ab46a980c87f91f93ee
CubicComet <cometa2k7@gmail.com>
2017-09-04 20:33:38 +0100

    Add initial solution

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,3 +1,17 @@
+from collections import defaultdict
+
+
 class School(object):
-    def __init__(self):
-        pass
+    def __init__(self, schoolname):
+        self.schoolname = schoolname
+        self._grades = defaultdict(set)
+
+    def grade(self, n):
+        return self._grades[n]
+
+    def add(self, name, grade):
+        self._grades[grade].update({name})
+
+    def sort(self):
+        return [(g, tuple(sorted(self._grades[g])))
+                for g in sorted(self._grades)]
