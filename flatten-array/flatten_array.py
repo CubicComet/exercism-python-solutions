dc7dace51dca59baf62dc41896e5c65bc6b8ad28
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-09-04 21:46:34 +0100

    Add initial solution

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,2 +1,15 @@
-def flatten():
-    pass
+def flatten(lst):
+    """Completely flatten an arbitrarily-deep list"""
+    return [*_flatten(lst)]
+
+
+def _flatten(lst):
+    """Generator for flattening arbitrarily-deep lists"""
+    if isinstance(lst, (list, tuple)):
+        for item in lst:
+            if item is None:
+                continue
+            else:
+                yield from _flatten(item)
+    else:
+        yield lst
