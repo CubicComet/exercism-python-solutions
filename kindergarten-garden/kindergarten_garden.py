c9d30e8873233adc84a6a7ed24423202e6538709
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-09-04 17:21:14 +0100

    Use unpacking for simpler code

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -6,8 +6,8 @@ PLANTS = {"C": "Clover", "G": "Grass", "R": "Radishes", "V": "Violets"}
 class Garden(object):
     def __init__(self, garden, students=CHILDREN):
         self.students = sorted(students)
-        rows = garden.split()
-        patches = [rows[0][i:i+2] + rows[1][i:i+2]
+        row1, row2 = garden.split()
+        patches = [row1[i:i+2] + row2[i:i+2]
                    for i in range(0,2*len(self.students),2)]
         self._garden = {s: [PLANTS[ch] for ch in p]
                         for s, p in zip(self.students, patches)}
