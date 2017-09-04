a2689ce5ebd7c5f26a178d9669061509f2341f1f
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-09-04 22:56:42 +0100

    Simplify _numeral helper function

--- roman-numerals/roman_numerals.py
+++ roman-numerals/roman_numerals.py
@@ -1,14 +1,14 @@
 def numeral(n):
     numerals = (n//1000) * "M"
-    numerals += _numeral(((n%500)//100, (n%1000)//500), ("C", "D", "M"))
-    numerals += _numeral(((n%50)//10, (n%100)//50), ("X", "L", "C"))
-    numerals += _numeral(((n%5), (n%10)//5), ("I", "V", "X"))
+    numerals += _numeral((n%1000)//100, "C", "D", "M")
+    numerals += _numeral((n%100)//10, "X", "L", "C")
+    numerals += _numeral(n%10, "I", "V", "X")
     return numerals
 
 
-def _numeral(nums, letters):
-    i, v, x = letters
-    if nums[1] == 1:
-        return i + x if nums[0] == 4 else v + nums[0] * i
+def _numeral(num, i, v, x):
+    n, m = divmod(num, 5)
+    if n == 1:
+        return i + x if m == 4 else v + m * i
     else:
-        return i + v if nums[0] == 4 else nums[0] * i
+        return i + v if m == 4 else m * i
