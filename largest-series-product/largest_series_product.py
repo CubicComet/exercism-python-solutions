6ff29d14b4996e0f00e5515726224911d84a516b
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-09-04 14:11:13 +0100

    Utilise unedited series.slices function

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -1,23 +1,15 @@
 import operator
 from functools import reduce
 
+import series # series.py symbolically linked in current folder
+
 
 def largest_product(s, n):
     if s and not s.isnumeric():
         raise ValueError("Invalid character in string")
 
-    return max(map(product, slices(s, n)))
+    return 1 if n == 0 else max(map(product, series.slices(s, n)))
 
 
 def product(lst):
     return reduce(operator.mul, lst, 1)
-
-
-def slices(s, n):
-    m = len(s)
-    if n > m:
-        raise ValueError("Slice cannot be larger than the string")
-    if n < 0:
-        raise ValueError("Slices must have a positive non-zero length")
-
-    return [list(map(int, s[i:i+n])) for i in range(m-n+1)]
