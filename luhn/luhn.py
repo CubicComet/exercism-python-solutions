3c8d905cbe49b75f902b0101f26a3955be8641e2
Nathan Parsons <cometa2k7@gmail.com>
2017-09-06 00:38:50 +0100

    Add initial solution

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,3 +1,20 @@
 class Luhn(object):
-    def __init__(self):
-        pass
+    def __init__(self, number):
+        self.number = number.replace(" ", "")
+
+    def is_valid(self):
+        if len(self.number) < 2 or not self.number.isdigit():
+            return False
+        numbers = map(int, reversed(self.number))
+        return sum(map(Luhn._double, enumerate(numbers))) % 10 == 0
+
+    @staticmethod
+    def _double(i_n):
+        i, n = i_n
+        if i % 2 == 0:
+            x = n
+        else:
+            x = 2 * n
+            if x > 9:
+                x -= 9
+        return x
