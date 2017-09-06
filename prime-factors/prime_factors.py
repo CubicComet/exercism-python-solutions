722db7c4c83b705811ada9678d11ee61c05e9327
Nathan Parsons <cometa2k7@gmail.com>
2017-09-06 11:40:58 +0100

    Add initial solution - very memory intensive

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,2 +1,11 @@
-def prime_factors():
-    pass
+import sieve
+
+
+def prime_factors(n):
+    primes = sieve.sieve(n)
+    factors = []
+    for p in primes:
+        while n % p == 0:
+            factors += [p]
+            n //= p
+    return factors
