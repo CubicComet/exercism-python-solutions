6b17f5824f05805d5823361ec0b5caefd00b8554
Nathan Parsons <cometa2k7@gmail.com>
2017-09-06 23:08:48 +0100

    Add initial solution

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,2 +1,15 @@
-def translate():
-    pass
+import re
+
+
+_REGEX = (r'^(?P<c>([^aeiou]?qu)|([^aeiouxy]+?)|([xy](?=[aeiou]))|())'
+          + r'(?P<v>(([xy][^aeiou]+)|([aeiou])).*)$')
+REGEX = re.compile(_REGEX)
+
+
+def translate(phrase):
+    return " ".join(map(_translate, phrase.split()))
+
+
+def _translate(word):
+    matches = REGEX.match(word)
+    return matches["v"] + matches["c"] + "ay"
