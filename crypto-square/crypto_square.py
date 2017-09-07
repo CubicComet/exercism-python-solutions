43b83e77ee278d103e53a2522945ea2c0015e77d
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-09-07 21:02:59 +0100

    Add initial solution

--- crypto-square/crypto_square.py
+++ crypto-square/crypto_square.py
@@ -1,2 +1,9 @@
-def encode():
-    pass
+import math
+
+
+def encode(s):
+    s = list(filter(str.isalnum, s.lower()))
+    size = math.ceil(math.sqrt(len(s)))
+    s += "." * (size**2 - len(s))
+    parts = [s[i*size:(i+1)*size] for i in range(size)]
+    return " ".join(map("".join, zip(*parts))).replace(".", "")
