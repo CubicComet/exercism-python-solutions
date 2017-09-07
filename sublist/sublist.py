f24a586940b2a4767200b96fd9d45a22b9083d94
Cubic Comet <cometa2k7@gmail.com>
2017-09-07 21:52:52 +0100

    Add initial solution

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,2 +1,17 @@
-def check_lists():
-    pass
+SUPERLIST = "superlist"
+SUBLIST = "sublist"
+EQUAL = "equal"
+UNEQUAL = "unequal"
+
+
+def check_lists(a, b):
+    if a == b:
+        return EQUAL
+    _a = ";;".join(map(str, a))
+    _b = ";;".join(map(str, b))
+    if _a in _b:
+        return SUBLIST
+    elif _b in _a:
+        return SUPERLIST
+    else:
+        return UNEQUAL
