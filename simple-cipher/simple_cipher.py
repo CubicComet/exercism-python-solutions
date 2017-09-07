0585dd36aa0bb5be3c4a5a4a36348922d738e5ce
Nathan Parsons <cometa2k7@gmail.com>
2017-09-07 18:13:05 +0100

    Add initial solution

--- simple-cipher/simple_cipher.py
+++ simple-cipher/simple_cipher.py
@@ -1,8 +1,36 @@
+import math
+import secrets
+from string import ascii_lowercase
+
+
 class Cipher(object):
-    def __init__(self):
-        pass
+    def __init__(self, key=None):
+        if not key:
+            key = Cipher._random_key()
+        if not key.isalpha() or not key.islower():
+            raise ValueError("Key must consist only of lowercase letters")
+        self.key = key
+        self._key = [ord(k)-97 for k in key]
+
+    def encode(self, s):
+        key = self._key * math.ceil(len(s)/len(self._key))
+        chars = [c for c in s.lower() if c in ascii_lowercase]
+        return "".join(Cipher._shift(c, k) for c, k in zip(chars, key))
+
+    def decode(self, s):
+        key = self._key * math.ceil(len(s)/len(self._key))
+        chars = [c for c in s.lower() if c in ascii_lowercase]
+        return "".join(Cipher._shift(c, -k) for c, k in zip(chars, key))
+
+    @staticmethod
+    def _shift(char, key):
+        return chr(97 + ((ord(char) - 97 + key) % 26))
+
+    @staticmethod
+    def _random_key(length=256):
+        return "".join(secrets.choice(ascii_lowercase) for _ in range(length))
 
 
-class Caesar(object):
+class Caesar(Cipher):
     def __init__(self):
-        pass
+        Cipher.__init__(self, "d")
