122c6e0950994db54952038f8f2a6c4179704bc4
Cubic Comet <cometa2k7@gmail.com>
2017-09-08 23:19:07 +0100

    Add initial solution

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,3 +1,9 @@
 class Matrix(object):
-    def __init__(self):
-        pass
+    def __init__(self, s):
+        self.rows = [list(map(int, row.split()))
+                       for row in s.split("\n")]
+
+    @property
+    def columns(self):
+        return [[row[i] for row in self.rows]
+                for i in range(len(self.rows[0]))]
