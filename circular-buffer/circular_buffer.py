29b5b83c3c3c74bd28915b195baf8aeee521990b
Cubic Comet <cometa2k7@gmail.com>
2017-09-08 21:09:54 +0100

    Add initial solution

--- circular-buffer/circular_buffer.py
+++ circular-buffer/circular_buffer.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class BufferFullException(Exception):
     pass
 
@@ -7,5 +10,23 @@ class BufferEmptyException(Exception):
 
 
 class CircularBuffer(object):
-    def __init__(self):
-        pass
+    def __init__(self, length):
+        self.buffer = collections.deque([], length)
+
+    def clear(self):
+        self.buffer.clear()
+
+    def read(self):
+        try:
+            return self.buffer.popleft()
+        except IndexError:
+            raise BufferEmptyException
+
+    def write(self, val):
+        if len(self.buffer) < self.buffer.maxlen:
+            self.buffer.append(val)
+        else:
+            raise BufferFullException
+
+    def overwrite(self, val):
+        self.buffer.append(val)
