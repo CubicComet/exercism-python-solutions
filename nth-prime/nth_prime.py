c9274f39238c13783a16c9acba2538057c8f5fb0
Cubic Comet <cometa2k7@gmail.com>
2017-09-15 22:47:25 +0100

    Add initial solution

--- nth-prime/nth_prime.py
+++ nth-prime/nth_prime.py
@@ -1,2 +1,19 @@
-def nth_prime():
-    pass
+import math
+
+
+def nth_prime(n):
+    if n < 1:
+        raise ValueError("n must be a positive integer")
+    _n = int(2 + 1.2 * n * math.log(n))
+    return list(primes(_n))[n-1]
+
+
+def primes(n):
+    if n < 2:
+        raise StopIteration
+    yield 2
+    not_prime = set()
+    for i in range(3, n+1, 2):
+        if i not in not_prime:
+            not_prime.update(range(i*i, n+1, i))
+            yield i
