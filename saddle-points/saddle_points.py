88f379da8f8d22c189d5c789b15f510dad2da3c2
Cubic Comet <cometa2k7@gmail.com>
2017-09-18 22:23:50 +0100

    Add initial solution

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -1,2 +1,11 @@
-def saddle_points():
-    pass
+def saddle_points(m):
+    mt = transpose(m)
+    if not m == transpose(mt):
+        raise ValueError
+    return set((i, j) for i, row in enumerate(m) for j, col in enumerate(mt)
+                if (row[j] == min(row) and col[i] == max(col))
+                or (row[j] == max(row) and col[i] == min(col)))
+
+
+def transpose(m):
+    return [list(col) for col in zip(*m)]
