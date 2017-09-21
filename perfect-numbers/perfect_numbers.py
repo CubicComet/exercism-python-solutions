2d2fb09c66f25216d73d98fb428c9dd2e3a9f7a0
Cubic Comet <cometa2k7@gmail.com>
2017-09-21 21:59:15 +0100

    Add initial solution

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,6 +1,13 @@
-def divisor_generator():
-    pass
+def divisor_generator(n):
+    big_factors = {1}
+    for i in range(2, n):
+        if i in big_factors:
+            break
+        if n % i == 0:
+            big_factors.add(n // i)
+            yield i
+    yield from big_factors
 
 
-def is_perfect():
-    pass
+def is_perfect(n):
+    return n == sum(divisor_generator(n))
