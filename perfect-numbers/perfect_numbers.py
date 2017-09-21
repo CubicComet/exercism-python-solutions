274a1b43a57f838f078d11ef71803d46f8fd34bf
Cubic Comet <cometa2k7@gmail.com>
2017-09-21 22:09:08 +0100

    Refactor to give faster solution

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,12 +1,12 @@
+from math import sqrt, ceil
+
+
 def divisor_generator(n):
-    big_factors = {1}
-    for i in range(2, n):
-        if i in big_factors:
-            break
+    yield 1
+    for i in range(2, ceil(sqrt(n))):
         if n % i == 0:
-            big_factors.add(n // i)
             yield i
-    yield from big_factors
+            yield n // i
 
 
 def is_perfect(n):
