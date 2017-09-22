7a67cd9525ab9ab2f7b1c17749a2289bf1422b92
Cubic Comet <cometa2k7@gmail.com>
2017-09-22 22:05:32 +0100

    Add alterative solution

--- palindrome-products/palindrome_products.py
+++ palindrome-products/palindrome_products.py
@@ -1,28 +1,37 @@
-import random
-from collections import defaultdict
+from collections import deque
+
+
+class FactorsOutOfBounds(Exception):
+    pass
 
 
 def largest_palindrome(max_factor, min_factor=0):
-    return _palindromes(max_factor, min_factor, max)
+    return _palindromes(max_factor, min_factor, deque.pop)
 
 
 def smallest_palindrome(max_factor, min_factor=0):
-    return _palindromes(max_factor, min_factor, min)
+    return _palindromes(max_factor, min_factor, deque.popleft)
 
 
-def _palindromes(max_factor, min_factor, minmax):
-    pals = defaultdict(set)
-
-    for i in range(min_factor, max_factor+1):
-        for j in range(min_factor, max_factor+1):
-            p = i * j
-            if is_palindrome(p):
-                pals[p].add(tuple(sorted([i,j])))
-
-    value = minmax(pals)
-    factors = random.choice(list(pals[value]))
-    return (value, factors)
+def _palindromes(max_factor, min_factor, pop_minmax):
+    pals = deque(filter(is_palindrome, range(min_factor**2, max_factor**2 + 1)))
+    while pals:
+        value = pop_minmax(pals)
+        try:
+            return (value, factor_pair(value, min_factor, max_factor))
+        except FactorsOutOfBounds:
+            pass
+    raise NoPalindromes("No palindrome products found in the specified range")
 
 
 def is_palindrome(n):
     return str(n) == str(n)[::-1]
+
+
+def factor_pair(n, min_factor, max_factor):
+    for i in range(max([1, min_factor]), max_factor+1):
+        if n % i == 0:
+            j = n // i
+            if min_factor <= j <= max_factor:
+                return (i, j)
+    raise FactorsOutOfBounds("There are no factors in that range")
