595ae6e9d6a6115cae7983f13bd0909a4fe1b527
Cubic Comet <cometa2k7@gmail.com>
2017-09-22 22:39:41 +0100

    Add initial solution

--- bracket-push/bracket_push.py
+++ bracket-push/bracket_push.py
@@ -1,2 +1,15 @@
-def check_brackets(string):
-    pass
+BRACKETS = ["()", "[]", "{}"]
+
+
+def check_brackets(s):
+    brackets = "".join(filter(is_bracket, s))
+    length = 0
+    while brackets and length != len(brackets):
+        length = len(brackets)
+        for pair in BRACKETS:
+            brackets = brackets.replace(pair, "")
+    return len(brackets) == 0
+
+
+def is_bracket(ch):
+    return ch in {"{", "}", "[", "]", "(", ")"}
