bbf1ef2211615543baba6411260c812b01f84c3c
Cubic Comet <cometa2k7@gmail.com>
2017-09-23 15:36:54 +0100

    Add initial solution

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -1,3 +1,21 @@
+import re
+
+
 class Phone(object):
-    def __init__(self):
-        pass
+    def __init__(self, n):
+        cleaned = "".join(filter(str.isnumeric, n))
+        m = re.match("^(1)?([2-9][0-9]{2})([2-9][0-9]{2})([0-9]{4})$", cleaned)
+        if m:
+            self._parts = [p if p else "" for p in m.groups()]
+        else:
+            self._parts = ("", "000", "000", "0000")
+
+    def area_code(self):
+        return self._parts[1]
+
+    def pretty(self):
+        return "({1}) {2}-{3}".format(*self._parts)
+
+    @property
+    def number(self):
+        return "{1}{2}{3}".format(*self._parts)
