fb0ee998dc27074eb1ba3c71e87dfc4b9060fd6f
Cubic Comet <cometa2k7@gmail.com>
2017-09-27 00:29:46 +0100

    Add initial solution

--- transpose/transpose.py
+++ transpose/transpose.py
@@ -1,2 +1,17 @@
-def transpose():
-    pass
+def transpose(inp):
+    rows = inp.split("\n")
+    max_len = max(map(len, rows))
+    inp_square = [row + ((max_len-len(row)) * " ") for row in rows]
+    new_rows = ["".join(tup) for tup in zip(*inp_square)]
+    return "\n".join(reversed(list(partial_rstrip(reversed(new_rows)))))
+
+
+def partial_rstrip(rows):
+    flag = False
+    for row in rows:
+        if not flag:
+            if not row.endswith(" "):
+                flag = True
+            yield row.rstrip()
+        else:
+            yield row
