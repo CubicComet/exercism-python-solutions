bb782f37a0f920c25eef626d47333ac4de62ec1f
Cubic Comet <cometa2k7@gmail.com>
2017-09-26 20:53:10 +0100

    Add initial solution

--- ocr-numbers/ocr_numbers.py
+++ ocr-numbers/ocr_numbers.py
@@ -1,6 +1,38 @@
-def number():
-    pass
+SEG = {'0': (' _ ', '| |', '|_|', '   '),
+       '1': ('   ', '  |', '  |', '   '),
+       '2': (' _ ', ' _|', '|_ ', '   '),
+       '3': (' _ ', ' _|', ' _|', '   '),
+       '4': ('   ', '|_|', '  |', '   '),
+       '5': (' _ ', '|_ ', ' _|', '   '),
+       '6': (' _ ', '|_ ', '|_|', '   '),
+       '7': (' _ ', '  |', '  |', '   '),
+       '8': (' _ ', '|_|', '|_|', '   '),
+       '9': (' _ ', '|_|', ' _|', '   ')}
+
+OCR = {v: k for k, v in SEG.items()}
 
 
-def grid():
-    pass
+def number(seg):
+    rows = [[row[i:i+3] for i in range(0, len(row), 3)] for row in seg]
+    try:
+        return "".join(digit(d) for d in zip(*rows))
+    except TypeError:
+        print(rows)
+
+
+def digit(d):
+    try:
+        return OCR[tuple(d)]
+    except KeyError:
+        if len(d) != 4 or not all(len(row) == 3 for row in d):
+            raise ValueError("Improperly formatted input")
+        else:
+            return "?"
+
+
+def grid(digits):
+    try:
+        segments = [SEG[n] for n in digits]
+    except KeyError:
+        raise ValueError("Unrecognised digit")
+    return ["".join(row) for row in zip(*segments)]
